b4e0bb805e3cc5e43884fa4c638d61fdf3b9c9a8
cor22982 <cor22982@uvg.edu.gt>
2024-03-27 20:24:13 -0600

    feat:rows test

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -4,10 +4,8 @@ import conn from './conn.js'
 export async function getAllPersonal() {
   try {
     const result = await conn.query('SELECT * FROM areas;');
-    result.then(response => {
-      console.log(response.rows)
-      client.end()
-    })
+    console.log('Connection to PostgreSQL is active.');
+    console.log(result.rows);
     return true;
   } catch (error) {
     console.error('Error checking connection to PostgreSQL:', error);
