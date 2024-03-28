9a54a3f6db14f12acaec3cdf494aa6102d42c890
cor22982 <cor22982@uvg.edu.gt>
2024-03-27 19:56:37 -0600

    fe

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -21,6 +21,7 @@ app.use(cors())
 app.get('/personal', async (req, res) => {
   try {
     const personal = await getAllPersonal()
+    console.log(personal)
     res.status(200).json({ data: personal }) // Se agrega el campo "data" para mostrar los datos
   } catch (error) {
     res.status(500).json({ error: error.message }) // Si hay un error, se muestra el mensaje de error
