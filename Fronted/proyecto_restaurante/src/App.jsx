f41a3b52ca90ef67ad957239428ed64da04ea54c
cor22982 <cor22982@uvg.edu.gt>
2024-04-06 17:01:53 -0600

    feat: Login and Register

--- Fronted/proyecto_restaurante/src/App.jsx
+++ Fronted/proyecto_restaurante/src/App.jsx
@@ -4,7 +4,7 @@ import Login from './LoginAndRegister/Login'
 function App() {
  
   return (
-    <Register/>
+    <Login/>
   )
 }
 
--- Fronted/proyecto_restaurante/src/LoginAndRegister/Login.jsx
+++ Fronted/proyecto_restaurante/src/LoginAndRegister/Login.jsx
@@ -1,15 +1,71 @@
 import React from 'react';
 import './Login.css';
+import { md5 } from 'js-md5'
+import { useState } from 'react'
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 import TextInput from '../Components/TextInput';
 import ButtonCustom from '../Components/ButtonCustom';
 const Login = () =>{
+  const [formState, setFormState] = useState({ username: '',password: ''})
+  const [errorMessage, setErrorMessage] = useState('')
+
+  
+  const setValue = (name, value) => {
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+  }
+
+  const handleClick = async () => {
+    const body = { }
+    body.username = formState.username
+    body.password = md5(formState.password)
+    const fetchOptions = {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const response = await fetch('https://cocina.web05.lol/login', fetchOptions)
+    // Aquí puedes agregar la lógica que desees ejecutar cuando el botón sea clicado
+    if (response.ok) {
+      console.log('success!')
+      setErrorMessage('')
+      return
+    }
+    setErrorMessage('Incorrect user or password')
+  }
   return ( 
     <div className="wrapper">
       <h1>Iniciar Sesion</h1>
-      <TextInput icono={faUser} placeholder="Usuario" type="text" />
-      <TextInput icono={faLock} placeholder="Constraseña" type="password" />
-      <ButtonCustom type='submit' nombre='Iniciar Sesion'></ButtonCustom>
+      {
+        errorMessage !== '' ? (
+          <div className='error-message' onClick={() => setErrorMessage('')}>
+            {errorMessage}
+          </div>
+        ) : null
+      }
+
+      <TextInput 
+        icono={faUser} 
+        placeholder="Usuario" 
+        type="text" 
+        value={formState.username} 
+        onChange={(value) => setValue('username', value)}
+        />
+      <TextInput 
+        icono={faLock} 
+        placeholder="Constraseña" 
+        type="password"
+        value={formState.password} 
+        onChange={(value)=> setValue('password',value)}
+        />
+      <ButtonCustom 
+        type='submit' 
+        nombre='Iniciar Sesion'
+        onClick={handleClick}></ButtonCustom>
     </div>
   );
 }
