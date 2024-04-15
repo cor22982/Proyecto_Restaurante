06e5a0f70dc39f254ec3e1e06b42854f33f09a25
ppguz <ppguzmanmayen@gmail.com>
2024-04-14 23:54:28 -0600

    Fix: column name error

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -260,7 +260,7 @@ export async function getTimeAVG(fecha_inicio, fecha_fin){
 
 export async function getQuejasByName(fecha_inicio, fecha_fin){
   try{
-    const result = await conn.query('SELECT p.nombre AS persona, COUNT(*) AS total_quejas FROM queja q JOIN personal p ON q.personal_id = p.id WHERE DATE(q.fecha_hora) BETWEEN $1 AND $2 GROUP BY p.nombre ORDER BY total_quejas DESC;', [fecha_inicio.toISOString(), fecha_fin.toISOString()])
+    const result = await conn.query(' SELECT p.nombre AS persona, COUNT(*) AS total_quejas FROM queja q JOIN personal p ON q.personal_id = p.id WHERE DATE(q.fecha_hora) BETWEEN $1 AND $2 GROUP BY p.nombre ORDER BY count(*) desc', [fecha_inicio.toISOString(), fecha_fin.toISOString()])
     return result
   }
   catch(error){
